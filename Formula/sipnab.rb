c6fead9e8f851f8efca2670bb06eca18c5ba821a
sipnab-release <n.brandinger@gmail.com>
2026-08-20 19:24:55 +0000

    sipnab: bump to 0.5.116

--- Formula/sipnab.rb
+++ Formula/sipnab.rb
@@ -2,16 +2,16 @@ class Sipnab < Formula
   desc "SIP & RTP capture, analysis, and security tool"
   homepage "https://sipnab.com"
   license any_of: ["MIT", "Apache-2.0"]
-  version "0.5.115"
+  version "0.5.116"
 
   on_macos do
     on_arm do
-      url "https://github.com/NormB/sipnab/releases/download/v0.5.115/sipnab-0.5.115-aarch64-apple-darwin.tar.gz"
-      sha256 "1d5f283d63c0ea5b5e4cf1621cdd5e8f39e6ff3caaec9e5481fda252f1d1fafe"
+      url "https://github.com/NormB/sipnab/releases/download/v0.5.116/sipnab-0.5.116-aarch64-apple-darwin.tar.gz"
+      sha256 "3aff883c628f9e4205a5e8ce114da485f6059658f98f431b781802279367322d"
     end
     on_intel do
-      url "https://github.com/NormB/sipnab/releases/download/v0.5.115/sipnab-0.5.115-x86_64-apple-darwin.tar.gz"
-      sha256 "03f60917821b697a9be461c3f1d85a7828eb7df9d7b91817eb4ad7407413a5cf"
+      url "https://github.com/NormB/sipnab/releases/download/v0.5.116/sipnab-0.5.116-x86_64-apple-darwin.tar.gz"
+      sha256 "aea6ea76045bd1f5f72ca95e70d6316b594a25d3d348a5b0f430a15505861c09"
     end
   end
 
@@ -19,12 +19,12 @@ class Sipnab < Formula
     # The gnu binaries dynamically link libpcap (and need it at runtime).
     depends_on "libpcap"
     on_arm do
-      url "https://github.com/NormB/sipnab/releases/download/v0.5.115/sipnab-0.5.115-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "82b3bd13cdd94b4bbbee1c91b3ab1ca38bf52c4c8e90c4cff4b6d4a587bba235"
+      url "https://github.com/NormB/sipnab/releases/download/v0.5.116/sipnab-0.5.116-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b2c6195d628599ad947401346bf826c987271ee3f3b7253e52ed06223e9774dd"
     end
     on_intel do
-      url "https://github.com/NormB/sipnab/releases/download/v0.5.115/sipnab-0.5.115-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "5a419c3b0acb6525e1b16fc1bde24f30e1e31f630768a7a80053fa3d318ed28c"
+      url "https://github.com/NormB/sipnab/releases/download/v0.5.116/sipnab-0.5.116-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "bda7a1463b6a85f5ad40cd74aa6be51b7dc7a8d9321d22fedc288b0da4512aa7"
     end
   end
 
